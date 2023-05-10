8328348997865bfdcc85d4b951c02ca543ea1680
smghouse97 <ghouseshadhik@gmail.com>
2023-05-10 13:21:22 +0530

    Changes to code

--- src/main/java/com/example/demo/WebAppApplication.java
+++ src/main/java/com/example/demo/WebAppApplication.java
@@ -4,18 +4,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class WebAppApplication extends SpringBootServletInitializer{
-	
-	@Override  
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)   
-	{  
-	return application.sources(WebAppApplication.class);  
-	}  
+public class WebAppApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebAppApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebAppApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppApplication.class, args);
+    }
+
+    @RestController
+    public static class WebAppController {
+
+        @GetMapping("/hello")
+        public String hello() {
+            return "<h1 style=\"color: hotpink; font-size: 40px; font: cursive;\"> Congratulations! You've successfully implemented the CI/CD pipeline and deployed a Java web application to AWS EC2 using Docker!</h1>";
+        }
+    }
 }

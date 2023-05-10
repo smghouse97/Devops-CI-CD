925d030ef731c8219417770c68d7aa746dd39137
smghouse97 <ghouseshadhik@gmail.com>
2023-05-10 13:26:52 +0530

    Changes to code

--- src/main/java/com/example/demo/WebAppApplication.java
+++ src/main/java/com/example/demo/WebAppApplication.java
@@ -4,27 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class WebAppApplication extends SpringBootServletInitializer {
+public class WebAppApplication extends SpringBootServletInitializer{
+	
+	@Override  
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)   
+	{  
+	return application.sources(WebAppApplication.class);  
+	}  
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(WebAppApplication.class);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppApplication.class, args);
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(WebAppApplication.class, args);
-    }
-
-    // @RestController
-    // public static class WebAppController {
-
-    //     @GetMapping("/hello")
-    //     public String hello() {
-    //         return "<h1 style=\"color: hotpink; font-size: 40px; font: cursive;\"> Congratulations! You've successfully implemented the CI/CD pipeline and deployed a Java web application to AWS EC2 using Docker!</h1>";
-    //     }
-    // }
 }
--- src/main/java/com/example/demo/WebAppController.java
+++ src/main/java/com/example/demo/WebAppController.java
@@ -6,20 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// @RestController
-// public class WebAppController {
-
-// 	@GetMapping("/hello")
-// 	public String hello() {
-// 		return "<h1 style=\"color: hotpink; font-size: 40px; font: cursive;\">Hello, World </h1>";
-// 	} 
-// }
 @RestController
-public static class WebAppController {
+public class WebAppController {
 
 	@GetMapping("/hello")
 	public String hello() {
-		return "<h1 style=\"color: hotpink; font-size: 40px; font: cursive;\"> Congratulations! You've successfully implemented the CI/CD pipeline and deployed a Java web application to AWS EC2 using Docker!</h1>";
-	}
+		return "<h1 style=\"color: hotpink; font-size: 40px; font: cursive;\">Hello, World you have successfully completed the CI/CD piepline </h1>";
+	} 
 }
-
